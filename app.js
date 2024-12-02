9c7a609678de44a08f47bc2da28efc936ba437f2
superscalar <65932360+superscalar@users.noreply.github.com>
2024-12-02 19:27:25 -0300

    app.js 2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hola Mundo!!');
+  res.send('Hello, World!!');
 });
 
 console.log("Done setting up server endpoints")
